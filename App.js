08051981155de0213063207fa68e1fd362acaa1d
Anveshi Shori <anveshi87@gmail.com>
2021-07-11 20:42:24 +0530

    commit wirelessLib

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import TransactionScreen from './Screens/BookTransaction';
+import SearchScreen from './Screens/Search';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+
+export default class App extends Component {
+  render(){
+    return (
+    
+          <AppNavigator/>
+    
   );
 }
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const TabNavigator = createBottomTabNavigator({
+Transaction: {screen:TransactionScreen},
+Search: {screen: SearchScreen}
+
+},
+
+{
+defaultNavigationOptions : ({navigation})=>({
+tabBarIcon:()=>{
+const routeName = navigation.state.routeName
+
+if(routeName === 'Transaction'){
+
+return (
+
+  <Image
+  source = {require('./assets/book.png')}
+  style={{width:40, height:40}}
+  
+  />
+)
+
+
+}
+else if(routeName === 'Search'){
+
+  return (
+
+    <Image
+    
+    source = {require('./assets/searchingbook.png')}
+    style={{width:40, height:40}}
+    />
+  )
+
+
+
+
+}
+
+
+}
+
+
+
+
+})
+
+
+
+})
+
+const AppNavigator = createAppContainer(TabNavigator)
